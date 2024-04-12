cf17b9f3715bf2dd5af4d6d15fcda9095a47e20b
Jakub <kiszczyckuba20@wp.pl>
2024-04-12 17:32:10 +0200

    Fix: Added fix for logs not saving in production env

---
+++ __tests__/db/index.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from "@jest/globals";
+
+describe("Fights", () => {
+  describe("Should throw", () => {
+    describe("No data passed", () => {
+      it(`This is example file for ci/cd to pass. Will add db tests after`, () => {
+        expect(2 + 2).toEqual(4);
+      });
+    });
+  });
+});
